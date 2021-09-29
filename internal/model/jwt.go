fa1df6e86fee3e0e27704452b1260938a61e772b
hsien <hsxsix@gmail.com>
2021-09-29 18:09:18 +0800

    feat: jwt model

---
+++ internal/model/jwt.go
@@ -0,0 +1,15 @@
+/**
+ * @File: jwt.go
+ * @Author: hsien
+ * @Description:
+ * @Date: 9/25/21 12:53 AM
+ */
+
+package model
+
+import "github.com/dgrijalva/jwt-go"
+
+type Claims struct {
+	UserId string `json:"user_id"`
+	jwt.StandardClaims
+}
